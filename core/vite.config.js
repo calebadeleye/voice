4412740428a5d99384f2ddc81dd62696e3c73302
Caleb <adeleyecaleb@gmail.com>
2025-10-28 23:50:18 +0100

    reconfigured vite config

--- core/vite.config.js
+++ core/vite.config.js
@@ -1,12 +1,18 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import path from 'path';
 
 export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
-            publicDirectory: 'core/build',
         }),
     ],
+    build: {
+        // Output compiled assets into /build folder (outside /core)
+        outDir: path.resolve(__dirname, '../build'),
+        emptyOutDir: true, // clears old build files before building new ones
+    },
+    publicDir: false, // disables copying /public content
 });
