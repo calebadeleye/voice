c36d431be3d151e0be2ae5128e03e0ed3e355286
Caleb <adeleyecaleb@gmail.com>
2025-10-28 23:11:03 +0100

    resized logo

--- core/resources/views/components/application-logo.blade.php
+++ core/resources/views/components/application-logo.blade.php
@@ -1,5 +1,1 @@
-<div class="flex items-center space-x-2">
-    <img src="{{ asset('logo.png') }}" 
-         alt="Naitalk Logo" 
-         class="h-10 w-auto max-h-12 object-contain">
-</div>
+<img src="{{ asset('logo.png') }}" alt="Naitalk" style="max-height: 50px; width: auto;">
