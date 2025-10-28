ba8b0369f29da5c7a6ca496df1033cf7327d13c4
Caleb <adeleyecaleb@gmail.com>
2025-10-28 23:04:59 +0100

    adjusted logo size

--- core/resources/views/components/application-logo.blade.php
+++ core/resources/views/components/application-logo.blade.php
@@ -1,1 +1,5 @@
-<img src="{{ asset('logo.png') }}" alt="Naitalk" class="h-10 w-auto">
+<div class="flex items-center space-x-2">
+    <img src="{{ asset('logo.png') }}" 
+         alt="Naitalk Logo" 
+         class="h-10 w-auto max-h-12 object-contain">
+</div>
